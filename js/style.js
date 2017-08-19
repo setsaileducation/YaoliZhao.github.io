08e7b93b6feae27a4570b2e36b603889f4e87f21
YaoliZhao <www. yaolizhaotangzhou@gmail.com>
2017-08-19 10:43:05 +0800

    Initial commit

--- js/style.js
+++
@@ -1,8 +0,0 @@
-window.onload=function () {
-	document.getElementById('input').onfocus=function () {
-		this.setAttribute('style','box-shadow:0 0 8px rgba(038, 198, 218, 0.85);');
-	};
-	document.getElementById('input').onblur=function () {
-		this.setAttribute('style','box-shadow:0 0 0 rgba(038, 198, 218, 0.85);');
-	};
-};
